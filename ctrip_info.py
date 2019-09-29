0b11cfd3932894cb770e6e0fb5e8b3f47ba18fef
yclageis18 <50047265+yclageis18@users.noreply.github.com>
2019-09-29 11:24:42 +0800

    欣欣旅游网
    ctrip_info.py  --> 省份加有切片,爬取河南地区所有景区, 去掉切片即可爬取全国景区

--- ctrip_info.py
+++ ctrip_info.py
@@ -178,6 +178,7 @@ class Ctrip_Area:
 
         pass
 
+    
     def page_info(self, wz_url):
         time.sleep(random.randint(5, 15))
         resp = requests.get(wz_url, headers=self.headers, verify=False)
